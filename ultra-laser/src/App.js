761ca0326f8df0ca59fcd659fc7cf6cbee229cd4
singhvishesh9484 <visheshsingh084@gmail.com>
2022-09-02 16:56:26 +0530

    some change

--- ultra-laser/src/App.js
+++ ultra-laser/src/App.js
@@ -4,12 +4,14 @@ import Counter from './Components/Counter';
 
 
 function App() {
+  // Home Page
   return (
     <div className="popupdiv">
       <h2>Welcome 👋</h2>
       <h1>Team Ultra-Laser</h1>
       <p><span style={{fontSize:"35px",color:"red"}}>Impossible</span> is Nothing</p>
       <br></br>
+
       <Counter/>
     </div>
   );
